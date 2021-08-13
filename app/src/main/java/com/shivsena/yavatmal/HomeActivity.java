f10a9dbdc5d653f2b405897d9e0f3105578144db
tanmaywaniGit <tanmaywani1432@gmail.com>
2021-08-13 15:17:42 +0530

    Shivsena Yavatmal

--- app/src/main/java/com/shivsena/yavatmal/HomeActivity.java
+++ app/src/main/java/com/shivsena/yavatmal/HomeActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
 
@@ -22,5 +23,11 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(new Intent(HomeActivity.this,NextActivity.class));
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
