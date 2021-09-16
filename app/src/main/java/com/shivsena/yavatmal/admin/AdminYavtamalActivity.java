ae0b53b8412c24d77c4b4b54a032dc27edfeeae4
tanmaywaniGit <tanmaywani1432@gmail.com>
2021-09-16 15:40:09 +0530

    Shivsena Yavatmal

--- app/src/main/java/com/shivsena/yavatmal/admin/AdminYavtamalActivity.java
+++ app/src/main/java/com/shivsena/yavatmal/admin/AdminYavtamalActivity.java
@@ -20,7 +20,7 @@ public class AdminYavtamalActivity extends AppCompatActivity {
     private FirebaseDatabase mDatabase;
     private DatabaseReference mRef;
 
-    private Button yavatmal_vidhansabha_login;
+    private Button yavatmal_vidhansabha_login,addPlace;
     private EditText yavatmal_vidhansabha_name,yavatmal_vidhansabha_phone;
     private Spinner yavatmal_vidhansabha_spinner;
     @Override
@@ -32,6 +32,11 @@ public class AdminYavtamalActivity extends AppCompatActivity {
         mRef = mDatabase.getReference().child(vidhansabha);
         initialise();
         this.yavatmal_vidhansabha_login.setOnClickListener(this::saveData);
+        this.addPlace.setOnClickListener(this::addPlace);
+    }
+
+    private void addPlace(View view) {
+        startActivity(new Intent(AdminYavtamalActivity.this,AdminPlaceActivity.class));
     }
 
     private void saveData(View view) {
@@ -61,9 +66,10 @@ public class AdminYavtamalActivity extends AppCompatActivity {
     }
 
     private void initialise() {
-        yavatmal_vidhansabha_login = findViewById(R.id.yavatmal_vidhansabha_login);
-        yavatmal_vidhansabha_name = findViewById(R.id.yavatmal_vidhansabha_name);
+        yavatmal_vidhansabha_login = findViewById(R.id.btn_addPlace);
+        yavatmal_vidhansabha_name = findViewById(R.id.et_addPlace);
         yavatmal_vidhansabha_phone = findViewById(R.id.yavatmal_vidhansabha_phone);
         yavatmal_vidhansabha_spinner = findViewById(R.id.yavatmal_vidhansabha_spinner);
+        addPlace = findViewById(R.id.btn_addPlace);
     }
 }
