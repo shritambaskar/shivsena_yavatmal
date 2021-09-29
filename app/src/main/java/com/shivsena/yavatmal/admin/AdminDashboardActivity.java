7870db73493d8bc29a28d2e1655ea32646e8822d
tanmaywaniGit <tanmaywani1432@gmail.com>
2021-09-29 14:15:44 +0530

    Shivsena Yavatmal

--- app/src/main/java/com/shivsena/yavatmal/admin/AdminDashboardActivity.java
+++ app/src/main/java/com/shivsena/yavatmal/admin/AdminDashboardActivity.java
@@ -3,15 +3,41 @@ package com.shivsena.yavatmal.admin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.shivsena.yavatmal.R;
 
 public class AdminDashboardActivity extends AppCompatActivity {
 
-
+    private Button addTaluka,addGav,addMember,deleteMember;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_dashboard);
+        initialise();
+        this.addTaluka.setOnClickListener(this::addTaluka);
+        this.addGav.setOnClickListener(this::addGav);
+        this.addMember.setOnClickListener(this::addMember);
+        this.deleteMember.setOnClickListener(this::deleteMember);
+    }
+
+    private void deleteMember(View view) {
+    }
+
+    private void addMember(View view) {
+    }
+
+    private void addGav(View view) {
+    }
+
+    private void addTaluka(View view) {
+    }
+
+    private void initialise() {
+        addTaluka.findViewById(R.id.btn_dash_taluka);
+        addGav.findViewById(R.id.btn_dash_gav);
+        addMember.findViewById(R.id.btn_dash_add_member);
+        deleteMember.findViewById(R.id.btn_dash_delete_member);
     }
 }
