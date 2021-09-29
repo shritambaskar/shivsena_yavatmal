2b1c4bbc5759d2a15e04bc837a531508b6faac2b
Shrikant Tambaskar <shri.tambaskar@gmail.com>
2021-09-29 14:56:05 +0530

    Shivsena Yavatmal

---
+++ app/src/main/java/com/shivsena/yavatmal/admin/AddGaonActivity.java
@@ -0,0 +1,16 @@
+package com.shivsena.yavatmal.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.shivsena.yavatmal.R;
+
+public class AddGaonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_gaon);
+    }
+}
