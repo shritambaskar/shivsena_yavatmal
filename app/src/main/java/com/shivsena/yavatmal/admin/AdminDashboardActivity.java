d7871b355aa15c69f08fad9dbc4290935769daf4
tanmaywaniGit <tanmaywani1432@gmail.com>
2021-09-29 14:22:10 +0530

    Shivsena Yavatmal

--- app/src/main/java/com/shivsena/yavatmal/admin/AdminDashboardActivity.java
+++ app/src/main/java/com/shivsena/yavatmal/admin/AdminDashboardActivity.java
@@ -2,6 +2,7 @@ package com.shivsena.yavatmal.admin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -26,6 +27,7 @@ public class AdminDashboardActivity extends AppCompatActivity {
     }
 
     private void addMember(View view) {
+        startActivity(new Intent(AdminDashboardActivity.this,AdminHomeActivity.class));
     }
 
     private void addGav(View view) {
