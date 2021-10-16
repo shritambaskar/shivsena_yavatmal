b2d1b9f3f33ad9d27508e0e6229d59a870d10d11
Shrikant Tambaskar <shri.tambaskar@gmail.com>
2021-10-16 14:04:34 +0530

    Shivsena Yavatmal

---
+++ app/src/main/java/com/shivsena/yavatmal/yavtmalvidhansabha/DigrasDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.shivsena.yavatmal.yavtmalvidhansabha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.shivsena.yavatmal.R;
+
+public class DigrasDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_digras_details);
+    }
+}
