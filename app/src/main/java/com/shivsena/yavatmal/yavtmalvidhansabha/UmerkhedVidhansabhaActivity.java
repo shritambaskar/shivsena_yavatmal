266c372790dc367c552db9148b2a04f69a2bc66a
tanmaywaniGit <tanmaywani1432@gmail.com>
2021-10-16 12:46:07 +0530

    Shivsena Yavatmal

--- app/src/main/java/com/shivsena/yavatmal/yavtmalvidhansabha/UmerkhedVidhansabhaActivity.java
+++ app/src/main/java/com/shivsena/yavatmal/yavtmalvidhansabha/UmerkhedVidhansabhaActivity.java
@@ -12,6 +12,11 @@ import com.shivsena.yavatmal.R;
 public class UmerkhedVidhansabhaActivity extends AppCompatActivity {
 
     private Button umerkhed_upjilha_pramukh,umerkhed_upjilha_sanghatika,umerkhed_upjilha_yuva_adhikari;
+    private Button umerkhed_taluka_pramukh,umerkhed_taluka_sanghatika,umerkhed_taluka_yuva_adhikari;
+    private Button umerkhed_uptaluka_pramukh,umerkhed_uptaluka_sanghatika,umerkhed_uptaluka_yuva_adhikari;
+    private Button umerkhed_vibhag_pramukh,umerkhed_vibhag_sanghatika,umerkhed_vibhag_yuva_adhikari;
+    private Button umerkhed_shakha_pramukh,umerkhed_shakha_sanghatika,umerkhed_shakha_yuva_adhikari;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,5 +52,22 @@ public class UmerkhedVidhansabhaActivity extends AppCompatActivity {
         umerkhed_upjilha_pramukh = findViewById(R.id.umerkhed_upjilha_pramukh);
         umerkhed_upjilha_sanghatika=findViewById(R.id.umerkhed_upjilha_sanghtika);
         umerkhed_upjilha_yuva_adhikari=findViewById(R.id.umerkhed_upjilha_yuva_adhikari);
+
+        umerkhed_taluka_pramukh = findViewById(R.id.umerkhed_taluka_pramukh);
+        umerkhed_taluka_sanghatika=findViewById(R.id.umerkhed_taluka_sanghtika);
+        umerkhed_taluka_yuva_adhikari=findViewById(R.id.umerkhed_taluka_yuva_adhikari);
+
+        umerkhed_uptaluka_pramukh = findViewById(R.id.umerkhed_uptaluka_pramukh);
+        umerkhed_uptaluka_sanghatika=findViewById(R.id.umerkhed_uptaluka_sanghtika);
+        umerkhed_uptaluka_yuva_adhikari=findViewById(R.id.umerkhed_uptaluka_yuva_adhikari);
+
+        umerkhed_vibhag_pramukh = findViewById(R.id.umerkhed_vibhag_pramukh);
+        umerkhed_vibhag_sanghatika = findViewById(R.id.umerkhed_vibhag_sanghtika);
+        umerkhed_vibhag_yuva_adhikari = findViewById(R.id.umerkhed_vibhag_yuva_adhikari);
+
+        umerkhed_shakha_pramukh = findViewById(R.id.umerkhed_shakha_pramukh);
+        umerkhed_shakha_sanghatika = findViewById(R.id.umerkhed_shakha_sanghtika);
+        umerkhed_shakha_yuva_adhikari = findViewById(R.id.umerkhed_shakha_yuva_adhikari);
+
     }
 }
