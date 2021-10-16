ebbc33bd3c0beb75a16b40ab2ce99919f8185d0f
Shrikant Tambaskar <shri.tambaskar@gmail.com>
2021-10-16 13:39:43 +0530

    Shivsena Yavatmal

---
+++ app/src/main/java/com/shivsena/yavatmal/yavtmalvidhansabha/UmerkhedTalukaDetailsActivity.java
@@ -0,0 +1,142 @@
+package com.shivsena.yavatmal.yavtmalvidhansabha;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.shivsena.yavatmal.R;
+import com.shivsena.yavatmal.adapter.ShivsenaAdapter;
+import com.shivsena.yavatmal.model.ShivsenaDetails;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UmerkhedTalukaDetailsActivity extends AppCompatActivity {
+
+    private Spinner talukaSpinner;
+    private RecyclerView talukaRecycler;
+
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mRef;
+    private ArrayList<String> spinnerList;
+    private ArrayAdapter<String> adapter;
+    private String vidhansabha,post,taluka;
+
+    private List<ShivsenaDetails> list;
+    private ShivsenaAdapter shiv_adapter;
+    private ChildEventListener mChildEventListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_umerkhed_taluka_details);
+        vidhansabha = getIntent().getStringExtra("vidhansabha");
+        post = getIntent().getStringExtra("post");
+        setTitle(post);
+
+        initialise();
+
+        talukaSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                taluka = talukaSpinner.getSelectedItem().toString().trim();
+
+                mDatabase = FirebaseDatabase.getInstance();
+                mRef = mDatabase.getReference(vidhansabha).child(taluka+" तालुका").child(post);
+                talukaRecycler.setHasFixedSize(true);
+                talukaRecycler.setLayoutManager(new LinearLayoutManager(UmerkhedTalukaDetailsActivity.this));
+                list = new ArrayList<>();
+                shiv_adapter = new ShivsenaAdapter(UmerkhedTalukaDetailsActivity.this,list);
+                talukaRecycler.setAdapter(shiv_adapter);
+
+                mChildEventListener = new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                        ShivsenaDetails data = snapshot.getValue(ShivsenaDetails.class);
+                        data.setUid(snapshot.getKey());
+                        list.add(data);
+                        shiv_adapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                    }
+
+                    @Override
+                    public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                };
+                mRef.addChildEventListener(mChildEventListener);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private void showData() {
+        mRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot item: snapshot.getChildren()){
+                    String data = item.getValue().toString();
+                    spinnerList.add(data);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(UmerkhedTalukaDetailsActivity.this, "Error "+error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+    }
+
+    private void initialise() {
+        talukaSpinner = findViewById(R.id.umerkhed_taluka_spinner);
+        talukaRecycler = findViewById(R.id.umerkhed_taluka_recycleView);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mDatabase = FirebaseDatabase.getInstance();
+        mRef = mDatabase.getReference(vidhansabha).child("तालुक्याचे नाव");
+        spinnerList = new ArrayList();
+        adapter = new ArrayAdapter<String>(UmerkhedTalukaDetailsActivity.this,
+                android.R.layout.simple_spinner_dropdown_item,spinnerList);
+        talukaSpinner.setAdapter(adapter);
+        showData();
+    }
+}
--- app/src/main/java/com/shivsena/yavatmal/yavtmalvidhansabha/UmerkhedVidhansabhaActivity.java
+++ app/src/main/java/com/shivsena/yavatmal/yavtmalvidhansabha/UmerkhedVidhansabhaActivity.java
@@ -25,22 +25,26 @@ public class UmerkhedVidhansabhaActivity extends AppCompatActivity {
         this.umerkhed_upjilha_pramukh.setOnClickListener(this::upjilhaPramukh);
         this.umerkhed_upjilha_sanghatika.setOnClickListener(this::upjilhaSanghatika);
         this.umerkhed_upjilha_yuva_adhikari.setOnClickListener(this::upjilhaYuvaAdhikari);
+
         this.umerkhed_taluka_pramukh.setOnClickListener(this::talukaPramukh);
         this.umerkhed_taluka_sanghatika.setOnClickListener(this::talukaSanghatika);
         this.umerkhed_taluka_yuva_adhikari.setOnClickListener(this::talukaYuvaAdhikari);
+
+        this.umerkhed_up_taluka_pramukh.setOnClickListener(this::upTalukaPramukh);
+        this.umerkhed_up_taluka_sanghatika.setOnClickListener(this::upTalukaSanghatika);
+        this.umerkhed_up_taluka_yuva_adhikari.setOnClickListener(this::upTalukaYuvaAdhikari);
+
+        this.umerkhed_vibhag_pramukh.setOnClickListener(this::vibhagPramukh);
+        this.umerkhed_vibhag_sanghatika.setOnClickListener(this::vibhagSanghatika);
+        this.umerkhed_vibhag_yuva_adhikari.setOnClickListener(this::vibhagYuvaAdhikari);
+
+        this.umerkhed_shakha_pramukh.setOnClickListener(this::shakhaPramukh);
+        this.umerkhed_shakha_sanghatika.setOnClickListener(this::shakhaSanghatika);
+        this.umerkhed_shakha_yuva_adhikari.setOnClickListener(this::shakhaYuvaAdhikari);
     }
 
-    private void talukaYuvaAdhikari(View view) {
-    }
-
-    private void talukaSanghatika(View view) {
-    }
-
-    private void talukaPramukh(View view) {
-    }
-
-    private void upjilhaYuvaAdhikari(View view) {
-        String post = "उपजिल्हा युवा अधिकारी";
+    private void upjilhaPramukh(View view) {
+        String post = "उपजिल्हा प्रमुख";
         Intent intent = new Intent(UmerkhedVidhansabhaActivity.this,UmerkhedDetailsActivity.class);
         intent.putExtra("post",post);
         startActivity(intent);
@@ -53,9 +57,118 @@ public class UmerkhedVidhansabhaActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    private void upjilhaPramukh(View view) {
-        String post = "उपजिल्हा प्रमुख";
+    private void upjilhaYuvaAdhikari(View view) {
+        String post = "उपजिल्हा युवा अधिकारी";
         Intent intent = new Intent(UmerkhedVidhansabhaActivity.this,UmerkhedDetailsActivity.class);
+        intent.putExtra("post",post);
+        startActivity(intent);
+    }
+
+    private void talukaPramukh(View view) {
+        String vidhansabha = "उमरखेड विधानसभा";
+        String post = "तालुका प्रमुख";
+        Intent intent = new Intent(UmerkhedVidhansabhaActivity.this,UmerkhedTalukaDetailsActivity.class);
+        intent.putExtra("vidhansabha",vidhansabha);
+        intent.putExtra("post",post);
+        startActivity(intent);
+    }
+
+    private void talukaSanghatika(View view) {
+        String vidhansabha = "उमरखेड विधानसभा";
+        String post = "तालुका संघटीका";
+        Intent intent = new Intent(UmerkhedVidhansabhaActivity.this,UmerkhedTalukaDetailsActivity.class);
+        intent.putExtra("vidhansabha",vidhansabha);
+        intent.putExtra("post",post);
+        startActivity(intent);
+    }
+
+    private void talukaYuvaAdhikari(View view) {
+        String vidhansabha = "उमरखेड विधानसभा";
+        String post = "तालुका युवा अधिकारी";
+        Intent intent = new Intent(UmerkhedVidhansabhaActivity.this,UmerkhedTalukaDetailsActivity.class);
+        intent.putExtra("vidhansabha",vidhansabha);
+        intent.putExtra("post",post);
+        startActivity(intent);
+    }
+
+    private void upTalukaPramukh(View view) {
+        String vidhansabha = "उमरखेड विधानसभा";
+        String post = "उपतालुका प्रमुख";
+        Intent intent = new Intent(UmerkhedVidhansabhaActivity.this,UmerkhedTalukaDetailsActivity.class);
+        intent.putExtra("vidhansabha",vidhansabha);
+        intent.putExtra("post",post);
+        startActivity(intent);
+    }
+
+
+    private void upTalukaSanghatika(View view) {
+        String vidhansabha = "उमरखेड विधानसभा";
+        String post = "उपतालुका संघटीका";
+        Intent intent = new Intent(UmerkhedVidhansabhaActivity.this,UmerkhedTalukaDetailsActivity.class);
+        intent.putExtra("vidhansabha",vidhansabha);
+        intent.putExtra("post",post);
+        startActivity(intent);
+    }
+
+    private void upTalukaYuvaAdhikari(View view) {
+        String vidhansabha = "उमरखेड विधानसभा";
+        String post = "उपतालुका युवा अधिकारी";
+        Intent intent = new Intent(UmerkhedVidhansabhaActivity.this,UmerkhedTalukaDetailsActivity.class);
+        intent.putExtra("vidhansabha",vidhansabha);
+        intent.putExtra("post",post);
+        startActivity(intent);
+    }
+
+    private void vibhagPramukh(View view) {
+        String vidhansabha = "उमरखेड विधानसभा";
+        String post = "विभाग प्रमुख";
+        Intent intent = new Intent(UmerkhedVidhansabhaActivity.this,UmerkhedTalukaDetailsActivity.class);
+        intent.putExtra("vidhansabha",vidhansabha);
+        intent.putExtra("post",post);
+        startActivity(intent);
+    }
+
+    private void vibhagSanghatika(View view) {
+        String vidhansabha = "उमरखेड विधानसभा";
+        String post = "विभाग संघटीका";
+        Intent intent = new Intent(UmerkhedVidhansabhaActivity.this,UmerkhedTalukaDetailsActivity.class);
+        intent.putExtra("vidhansabha",vidhansabha);
+        intent.putExtra("post",post);
+        startActivity(intent);
+    }
+
+    private void vibhagYuvaAdhikari(View view) {
+        String vidhansabha = "उमरखेड विधानसभा";
+        String post = "विभाग युवा अधिकारी";
+        Intent intent = new Intent(UmerkhedVidhansabhaActivity.this,UmerkhedTalukaDetailsActivity.class);
+        intent.putExtra("vidhansabha",vidhansabha);
+        intent.putExtra("post",post);
+        startActivity(intent);
+    }
+
+    private void shakhaPramukh(View view) {
+        String vidhansabha = "उमरखेड विधानसभा";
+        String post = "शाखा प्रमुख";
+        Intent intent = new Intent(UmerkhedVidhansabhaActivity.this,UmerkhedTalukaDetailsActivity.class);
+        intent.putExtra("vidhansabha",vidhansabha);
+        intent.putExtra("post",post);
+        startActivity(intent);
+    }
+
+    private void shakhaSanghatika(View view) {
+        String vidhansabha = "उमरखेड विधानसभा";
+        String post = "शाखा संघटीका";
+        Intent intent = new Intent(UmerkhedVidhansabhaActivity.this,UmerkhedTalukaDetailsActivity.class);
+        intent.putExtra("vidhansabha",vidhansabha);
+        intent.putExtra("post",post);
+        startActivity(intent);
+    }
+
+    private void shakhaYuvaAdhikari(View view) {
+        String vidhansabha = "उमरखेड विधानसभा";
+        String post = "शाखा युवा अधिकारी";
+        Intent intent = new Intent(UmerkhedVidhansabhaActivity.this,UmerkhedTalukaDetailsActivity.class);
+        intent.putExtra("vidhansabha",vidhansabha);
         intent.putExtra("post",post);
         startActivity(intent);
     }
