4a20dc52fba37e5158dc846a998bf03458f56da0
tanmaywaniGit <tanmaywani1432@gmail.com>
2021-10-16 14:48:00 +0530

    Shivsena Yavatmal

--- app/src/main/java/com/shivsena/yavatmal/yavtmalvidhansabha/DigrasVidhansabhaActivity.java
+++ app/src/main/java/com/shivsena/yavatmal/yavtmalvidhansabha/DigrasVidhansabhaActivity.java
@@ -12,6 +12,5 @@ public class DigrasVidhansabhaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_digras_vidhansabha);
-        Hello
     }
 }
