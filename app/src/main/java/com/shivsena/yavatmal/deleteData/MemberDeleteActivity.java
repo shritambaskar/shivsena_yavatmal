52188e816f305305063ad0c8aaab7fa897c3dbd9
tanmaywaniGit <tanmaywani1432@gmail.com>
2021-10-23 16:42:17 +0530

    Shivsena Yavatmal

--- app/src/main/java/com/shivsena/yavatmal/deleteData/MemberDeleteActivity.java
+++ app/src/main/java/com/shivsena/yavatmal/deleteData/MemberDeleteActivity.java
@@ -1,16 +1,55 @@
 package com.shivsena.yavatmal.deleteData;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.shivsena.yavatmal.R;
+import com.shivsena.yavatmal.admin.AddGaonActivity;
+
+import java.util.ArrayList;
 
 public class MemberDeleteActivity extends AppCompatActivity {
+
+    private Spinner deleteSpinnerVidhansabha, deleteSpinnerPost,deleteSpinnerTaluka;
+    private Button deleteGo;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mRef;
+    private ArrayList<String> spinnerList;
+    private ArrayAdapter<String> adapter;
+    private String myVidhanSabha,myTaluka,post,vidhansabha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_delete);
+        initialise();
+        this.deleteGo.setOnClickListener(this::deleteData);
+
+    }
+
+    private void deleteData(View view) {
+
+    }
+
+
+    private void initialise() {
+        deleteSpinnerVidhansabha = findViewById(R.id.delete_spinner_vidhansabha);
+        deleteSpinnerPost = findViewById(R.id.delete_spinner_post);
+        deleteSpinnerTaluka = findViewById(R.id.delete_spinner_taluka);
+        deleteGo = findViewById(R.id.btn_delete_go);
     }
 }
---
+++ app/src/main/java/com/shivsena/yavatmal/deleteData/ShowDataActivity.java
@@ -0,0 +1,16 @@
+package com.shivsena.yavatmal.deleteData;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.shivsena.yavatmal.R;
+
+public class ShowDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_data);
+    }
+}
