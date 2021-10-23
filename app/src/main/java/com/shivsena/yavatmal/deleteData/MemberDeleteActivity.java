63d34cc4205a7e7d8a63d4c16439c8c343beb404
Shrikant Tambaskar <shri.tambaskar@gmail.com>
2021-10-23 17:33:26 +0530

    Shivsena Yavatmal

--- app/src/main/java/com/shivsena/yavatmal/deleteData/MemberDeleteActivity.java
+++ app/src/main/java/com/shivsena/yavatmal/deleteData/MemberDeleteActivity.java
@@ -19,6 +19,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.shivsena.yavatmal.R;
 import com.shivsena.yavatmal.admin.AddGaonActivity;
+import com.shivsena.yavatmal.admin.AdminYavtamalActivity;
 
 import java.util.ArrayList;
 
@@ -37,12 +38,102 @@ public class MemberDeleteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_delete);
         initialise();
+        deleteSpinnerVidhansabha.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                vidhansabha = deleteSpinnerVidhansabha.getSelectedItem().toString().trim();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        deleteSpinnerPost.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                post = deleteSpinnerPost.getSelectedItem().toString().trim();
+                if(post.equals("उपजिल्हा प्रमुख") || post.equals("उपजिल्हा संघटीका") || post.equals("उपजिल्हा युवा अधिकारी")){
+                    deleteSpinnerTaluka.setVisibility(View.INVISIBLE);
+                }
+                if(post.equals("तालुका प्रमुख") || post.equals("तालुका संघटीका") || post.equals("तालुका युवा अधिकारी")
+                        || post.equals("उपतालुका प्रमुख") || post.equals("उपतालुका संघटीका")
+                        || post.equals("उपतालुका युवा अधिकारी")|| post.equals("विभाग प्रमुख")
+                        || post.equals("विभाग संघटीका")|| post.equals("विभाग युवा अधिकारी")
+                        || post.equals("शाखा प्रमुख")|| post.equals("शाखा संघटीका")
+                        || post.equals("शाखा युवा अधिकारी")){
+
+                    deleteSpinnerTaluka.setVisibility(View.VISIBLE);
+
+                    mDatabase = FirebaseDatabase.getInstance();
+                    mRef = mDatabase.getReference(vidhansabha).child("तालुक्याचे नाव");
+                    spinnerList = new ArrayList();
+                    adapter = new ArrayAdapter<String>(MemberDeleteActivity.this,
+                            android.R.layout.simple_spinner_dropdown_item,spinnerList);
+                    deleteSpinnerTaluka.setAdapter(adapter);
+                    showData();
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        deleteSpinnerTaluka.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                myTaluka = deleteSpinnerTaluka.getSelectedItem().toString().trim();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+         this.deleteGo.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+
+                 if(vidhansabha!= null && post!=null) {
+                     Intent intent = new Intent(MemberDeleteActivity.this, ShowDataActivity.class);
+                     intent.putExtra("vidhansabha", vidhansabha);
+                     intent.putExtra("post", post);
+                     startActivity(intent);
+                 }
+                 if(vidhansabha!= null && post!=null&&myTaluka!=null) {
+                     Intent intent = new Intent(MemberDeleteActivity.this, ShowDataActivity.class);
+                     intent.putExtra("vidhansabha", vidhansabha);
+                     intent.putExtra("post", post);
+                     intent.putExtra("taluka",myTaluka);
+                     startActivity(intent);
+                 }
+             }
+         });
+        }
 
 
+    private void showData() {
+        mRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot item : snapshot.getChildren()) {
+                    String data = item.getValue().toString();
+                    spinnerList.add(data);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(MemberDeleteActivity.this, "Error " + error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+        });
     }
-
-
-
     private void initialise() {
         deleteSpinnerVidhansabha = findViewById(R.id.delete_spinner_vidhansabha);
         deleteSpinnerPost = findViewById(R.id.delete_spinner_post);
--- app/src/main/java/com/shivsena/yavatmal/deleteData/ShowDataActivity.java
+++ app/src/main/java/com/shivsena/yavatmal/deleteData/ShowDataActivity.java
@@ -3,6 +3,7 @@ package com.shivsena.yavatmal.deleteData;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.shivsena.yavatmal.R;
 
@@ -12,5 +13,17 @@ public class ShowDataActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_data);
+        TextView dummy = findViewById(R.id.dummy);
+
+        String vidhansabha = getIntent().getStringExtra("vidhansabha");
+        String post = getIntent().getStringExtra("post");
+        String taluka = getIntent().getStringExtra("taluka");
+        if(taluka == null){
+
+            dummy.setText(vidhansabha+","+post);
+        }else {
+
+            dummy.setText(vidhansabha + "," + post + "," + taluka);
+        }
     }
 }
